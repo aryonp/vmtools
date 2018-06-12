209455d15b89d3ac66a684881eec0b2112842c3f
aryonp <1676948+aryonp@users.noreply.github.com>
2018-06-12 15:24:17 +0700

    Update install_vmtools.sh

--- install_vmtools.sh
+++ install_vmtools.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 clear
 echo "** Cleanup, update everything and install dependencies"
-sudo apt remove -y open-vm-tools --purge && sudo apt update && sudo apt upgrade -y && sudo apt install -y gcc binutils make &> /dev/null
+sudo apt remove -y open-vm-tools --purge && sudo apt update && sudo apt upgrade -y && sudo apt install -y gcc binutils make
 
 echo "** Preparing ... "
-{
-  sudo mkdir /mnt/cdrom
-  sudo mount /dev/cdrom /mnt/cdrom 
-  ls /mnt/cdrom
-  tar xzvf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-  cd /tmp/vmware-tools-distrib/
-} &> /dev/null
+sudo mkdir /mnt/cdrom
+sudo mount /dev/cdrom /mnt/cdrom 
+ls /mnt/cdrom
+tar xzvf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
+cd /tmp/vmware-tools-distrib/
 
 echo "** Install the VMTools"
 sudo ./vmware-install.pl
